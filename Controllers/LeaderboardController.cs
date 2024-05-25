a2a9772e5ad3c7fbcb5a98a43eeeaea0052f371c
Han Hua <lhanhua01@gmail.com>
2024-05-25 20:33:07 +0800

    Completed Profile Page

---
+++ Controllers/LeaderboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AngelHack.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        private readonly AppDbContext _dbCtx;
+
+        public LeaderboardController(AppDbContext dbContext)
+        {
+            _dbCtx = dbContext;
+        }
+
+
+        public IActionResult Index()
+        {
+			DbSet<AppUser> dbs = _dbCtx.AppUser;
+            List<AppUser> users = dbs.ToList();
+			return View();
+        }
+    }
+}
