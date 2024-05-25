9af3aecca647c74c1fe57f527f5ea49482e2d6e5
Han Hua <lhanhua01@gmail.com>
2024-05-25 14:44:19 +0800

    Completed Login to Server

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AngelHack.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
